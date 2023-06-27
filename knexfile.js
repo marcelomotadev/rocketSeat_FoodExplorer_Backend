15b8b3727c88f2d2039c44f6385caf9eb345c8b6
marcelo <marcelomotadev@gmail.com>
2023-06-27 01:39:25 -0300

    first commit

---
+++ knexfile.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: path.resolve(__dirname, "src", "database", "database.db")
+    },
+    // para habilitar que ao deletar um item ele deleta em cascata seus ingredientes
+    pool: {
+      afterCreate: (conn, cb) => conn.run("PRAGMA foreign_keys = ON", cb)
+    },
+    migrations: {
+      directory: path.resolve(__dirname, "src", "database", "knex", "migrations")
+    },
+    useNullAsDefault: true
+  },
+
+};
---
+++ src/utils/AppError.js
@@ -0,0 +1,13 @@
+// trantando erros
+class AppError {
+  message;
+  statusCode;
+
+  constructor(message, statusCode = 400) {
+    this.message = message;
+    this.statusCode = statusCode;
+  }
+
+}
+
+module.exports = AppError
