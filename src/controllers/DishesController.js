a6565f6bacf176b85512fb8e97a5b4f3efaf9555
marcelo <marcelomotadev@gmail.com>
2023-06-27 13:17:23 -0300

    update comments

--- src/controllers/DishesController.js
+++ src/controllers/DishesController.js
@@ -16,25 +16,22 @@ class DishesController {
         }
 
         // Requesting image filename
-        // const imageFileName = request.file.filename;
+        const imageFileName = request.file.filename;
 
         // Instantiating diskStorage
         const diskStorage = new DiskStorage()
 
         // Saving image file
-        // const filename = await diskStorage.saveFile(imageFileName);
+        const filename = await diskStorage.saveFile(imageFileName);
 
         // Inserting the infos into the database
         const [ dish_id ] = await knex("dishes").insert({
-            // image: filename,
+            image: filename,
             title,
             description,
             price,
             category,
         });
-
-        // Checking if dish has only one ingredient and inserting the infos into the database
-        // const hasOnlyOneIngredient = typeof(ingredients) === "string";
 
         const ingredientsInsert = ingredients.map(name => {
             return {
@@ -42,21 +39,6 @@ class DishesController {
                 dish_id
             }
         });
-
-        // if (hasOnlyOneIngredient) {
-        //     ingredientsInsert = {
-        //         name: ingredients,
-        //         dish_id
-        //     }
-
-        // } else if (ingredients.length > 1) {
-        //     ingredientsInsert = ingredients.map(name => {
-        //         return {
-        //             name,
-        //             dish_id
-        //         }
-        //     });
-        // }
 
         await knex("ingredients").insert(ingredientsInsert);
 
